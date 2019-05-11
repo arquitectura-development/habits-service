67a603402c281d0ad9ca1f87831b4aaba3bc6833
AlanAlv <37889366+AlanAlv@users.noreply.github.com>
2019-05-11 12:05:21 -0500

    db

--- src/main/kotlin/pl/alan/services/SampleSpringKotlinMicroserviceApplication.kt
+++ src/main/kotlin/pl/alan/services/SampleSpringKotlinMicroserviceApplication.kt
@@ -29,7 +29,7 @@ fun main(args: Array<String>) {
     //Database.connect("jdbc:mysql://127.0.0.1:3306/habits", driver = "com.mysql.jdbc.Driver")
 
 
-    Database.connect("jdbc:mysql://sql3.freemysqlhosting.net", driver = "com.mysql.jdbc.Driver",
+    Database.connect("jdbc:mysql://sql3.freemysqlhosting.net/sql3291433", driver = "com.mysql.jdbc.Driver",
             user = "sql3291433", password = "p9j5YyqdNy")
 
 
